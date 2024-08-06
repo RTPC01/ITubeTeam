3f20654ba8ee16f460306b6145af9ff37aa68571
RTPC <hyhy9501@naver.com>
2024-08-06 22:24:12 +0900

    commit initial file

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Register from './components/Register';
+import Login from './components/Login';
+import Home from "./components/Home";
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                {/* 다른 라우트를 추가하세요 */}
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
