085c438d6c05525c30f0dd876380fe1b80996e4f
RTPC <hyhy9501@naver.com>
2024-08-13 20:13:41 +0900

    WatchVideo.js middle commit with jwt token test commit

--- frontend/src/components/Context/AuthContext.js
+++ frontend/src/components/Context/AuthContext.js
@@ -7,17 +7,24 @@ export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
 
     useEffect(() => {
-        const fetchUser = async () => {
-            try {
-                const response = await api.get('/auth/getCurrentUser');
-                setUser(response.data);
-            } catch (error) {
-                console.error('User is not authenticated', error);
-            }
-        };
-
-        fetchUser();
+        if (localStorage.getItem("token") != null) {
+            const fetchUser = async () => {
+                try {
+                    const response = await api.get('/auth/getCurrentUser');
+                    setUser(response.data);
+                } catch (error) {
+                    localStorage.removeItem('token');
+                    console.log(localStorage.getItem('token'));
+                    console.error('User is not authenticated', error);
+                }
+            };
+            fetchUser();
+        }
     }, []);
+
+    useEffect(() => {
+        console.log(user);
+    }, [user]);
 
     return (
         <AuthContext.Provider value = {{ user }}>
